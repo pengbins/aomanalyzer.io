8e21d85a59f5b47aad875832962e5939f3d80fbd
peng bin <pengbins@163.com>
2019-09-17 11:38:38 +0800

    Add video only btn in slide mode

--- dist/worker.js
+++ dist/worker.js
@@ -4,9 +4,9 @@ self.importScripts('ffmpeg.js');
 onmessage = function(e) {
   var files = e.data;
   var retrun;
-  console.log(files);
+  console.log(files, typeof(files));
   ffmpeg_run({
-    arguments: ['-v', 'panic','-i', '/input/' + files[0].name, '-vcodec', 'copy', 'out.mp4'],
+    arguments: ['-v', 'panic', '-i', '/input/' + files[0].name, '-vcodec', 'copy', "-an",'out.mp4'],
     files: files,
     retrun:retrun
   }, function(results) {
@@ -14,6 +14,4 @@ onmessage = function(e) {
     var blobURL = URL.createObjectURL(blob);
     self.postMessage(blobURL);
   });
-
 }
-
